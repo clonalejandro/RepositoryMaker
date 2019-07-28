d3a0df1ed46ebd95430ecc2700c6ecf24139a039
clonalejandro <contact@clonalejandro.me>
2019-07-28 19:21:12 +0200

    Create __init__.py

---
+++ assets/__init__.py
@@ -0,0 +1,18 @@
+
+# -*- coding=utf-8 -*-
+
+# /**
+# * Created by alejandrorioscalera
+# * On 21/9/17
+# *
+# * -- SOCIAL NETWORKS --
+# *
+# * GitHub: https://github.com/clonalejandro or @clonalejandro
+# * Website: https://clonalejandro.me/
+# * Twitter: https://twitter.com/clonalejandro11/ or @clonalejandro11
+# * Keybase: https://keybase.io/clonalejandro/
+# *
+# * -- LICENSE --
+# *
+# * All rights reserved for clonalejandro ©RepositoryMaker 2017 / 2018
+# */
